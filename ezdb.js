99aa5bfa4332517c5a730afb13409f319e65aeba
Yuga Cohler <yuga.cohler@coinbase.com>
2024-09-03 12:40:08 -0400

    Initial implementation with PouchStore

---
+++ ezdb.js
@@ -0,0 +1,5 @@
+/**
+ * @typedef {Object} Ezdb
+ * @property {function(string, any): Promise<void>} set - Sets a value in the store
+ * @property {function(string): Promise<any>} get - Gets a value from the store
+ */
---
+++ pouch_store.js
@@ -0,0 +1,51 @@
+import PouchDB from "pouchdb";
+
+/**
+ * DB class for interacting with PouchDB
+ * @class
+ * @implements {Ezdb}
+ */
+class PouchStore {
+  /**
+   * Creates an instance of DB
+   * @param {string} dbName - The name of the database
+   */
+  constructor(dbName) {
+    this.db = new PouchDB(dbName);
+  }
+
+  /**
+   * Sets a value in the database
+   * @param {string} key - The key to set
+   * @param {any} value - The value to set
+   * @returns {Promise<void>}
+   * @throws {Error} If there's an error setting the value
+   */
+  async set(key, value) {
+    try {
+      await this.db.put({
+        _id: key,
+        value: value,
+      });
+    } catch (error) {
+      console.error("Error setting value:", error);
+      throw error;
+    }
+  }
+
+  /**
+   * Gets a value from the database
+   * @param {string} key - The key to get
+   * @returns {Promise<any>} The value associated with the key
+   * @throws {Error} If there's an error getting the value
+   */
+  async get(key) {
+    try {
+      const doc = await this.db.get(key);
+      return doc.value;
+    } catch (error) {
+      console.error("Error getting value:", error);
+      throw error;
+    }
+  }
+}
